4097e21ca407bf2fa1714256d152604bdf2734de
Olivier Locard <free.oloc@gmail.com>
2021-07-26 17:30:07 +0200

    Ajout tirage au sort

--- cartomancie.py
+++ cartomancie.py
@@ -3,5 +3,9 @@
 #
 # Olivier Locard
 
+from random import randint
+
 cards_list = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'Valet', 'Dame', 'Roi', 'As']
 colors_list = ['Carreau', 'Coeur', 'Pique', 'Trèfle']
+
+print("Carte tirée au hasard : " + cards_list[randint(0, 12)] + " de " + colors_list[randint(0, 3)])
