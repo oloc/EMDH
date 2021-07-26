20ef8e15d0ddaa9aa968aec3e97681b425e5ee27
Olivier Locard <free.oloc@gmail.com>
2021-07-26 18:14:11 +0200

    Chiffrement de l'image

--- chiffre_image.py
+++ chiffre_image.py
@@ -3,6 +3,18 @@
 #
 # Olivier Locard
 
+from random import randint
+
 from PIL import Image
 
 img = Image.open(input("Quelle image voulez-vous chiffrer ?\n"))
+
+width, height = img.size
+pixels = img.load()
+
+for x in range(width):
+    for y in range(height):
+        (r, g, b, a) = pixels[x, y]
+        pixels[x, y] = (int(r / 10), randint(10, 250), randint(10, 250))
+
+img.save("oloc.png")
