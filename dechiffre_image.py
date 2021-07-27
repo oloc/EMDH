3f72c51211f4b3c6d9d1295987c735df248cfde0
Olivier Locard <free.oloc@gmail.com>
2021-07-27 15:40:56 +0200

    On adapte le déchiffrement selon l'extension de l'image

--- dechiffre_image.py
+++ dechiffre_image.py
@@ -5,14 +5,19 @@
 
 from PIL import Image
 
-img = Image.open(input("Quelle image voulez-vous déchiffrer ?\n"))
+image_name = input("Quelle image voulez-vous déchiffrer ?\n")
+image_ext = image_name[-4:].lower()
+img = Image.open(image_name)
 
 width, height = img.size
 pixels = img.load()
 
 for x in range(width):
     for y in range(height):
-        (r, g, b, a) = pixels[x, y]
-        pixels[x, y] = (r * 10, 0, 0, a)
+        if image_ext == '.png':
+            (r, g, b, a) = pixels[x, y]
+        elif image_ext == '.jpg':
+            (r, g, b) = pixels[x, y]
+        pixels[x, y] = (r * 10, 0, 0)
 
 img.show()
